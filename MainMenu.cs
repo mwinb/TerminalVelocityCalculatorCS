8743c709228e7fb6539389506cb6447cc7a1aae0
mwinb <mwinberry0101@gmail.com>
2019-02-19 21:07:06 -0800

    Created Main Menu Class

---
+++ MainMenu.cs
@@ -0,0 +1,16 @@
+using System;
+public class MainMenu
+{
+    public MainMenu()
+    {
+        String welcome = "Welcome to Terminal Velocity Calculator";
+        String calculate = "Press 1 to calculate new terminal velocity";
+        String exit = "Press any other key to exit";
+    }
+
+    public printMenu()
+    {
+        Console.Write("%s\n%s\n%s\n", welcome, calculate, exit);
+    
+    }
+}
